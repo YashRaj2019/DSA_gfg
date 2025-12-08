d571b170b8f6120856a4c2fd3b2da30621e458d6
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2025-12-08 21:12:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    vector<int> firstAndLast(int x, vector<int> &arr) {
+        // Code here
+        int n=arr.size();
+        
+        int mid, start=0, end = n-1, left_index=-1, right_index=-1;
+        
+        while(start<=end){
+            mid=start + (end-start)/2;
+            
+            if(arr[mid]==x){
+                left_index=mid;
+                end=mid-1;
+            }
+            
+            else if(arr[mid]>x){
+                end=mid-1;
+            }
+            
+            else{
+                start=mid+1;
+            }
+        }
+        
+        start=0;
+        end=arr.size()-1;
+        
+        
+        while(start<=end){
+            mid=start + (end-start)/2;
+            
+            if(arr[mid]==x){
+                right_index=mid;
+                start=mid+1;
+            }
+            
+            else if(arr[mid]>x){
+                end=mid-1;
+            }
+            
+            else{
+                start=mid+1;
+            }
+        }
+        
+        if(left_index==-1){
+            return {-1};
+        }
+        return {left_index, right_index};
+    }
+};
