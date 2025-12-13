a82922b50a0e72101dd2678b798f6a6f20cc61c3
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2025-12-13 12:40:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -2,35 +2,57 @@ class Solution {
   public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         // code here
-        
         int n=a.size();
         int m=b.size();
         
-        vector<int> union_arr(n+m);
-        
-        for(int i=0;i<n;i++){
-            union_arr[i]=a[i];
-        }
-        
-        for(int i=0;i<m;i++){
-            union_arr[n+i]=b[i];
-        }
-        
-        sort(union_arr.begin(), union_arr.end());
-        
         vector<int>result;
         
-        result.push_back(union_arr[0]);
+        int i=0, j=0;
         
-        for(int i=1;i<n+m;i++){
-            if(union_arr[i]!=union_arr[i-1]){
-                result.push_back(union_arr[i]);
+        while(i<n && j<m){
+            
+            
+            if(i>0 && a[i]==a[i-1]){
+                i++;
+                continue;
+            }
+            
+            if(j>0 && b[j]==b[j-1]){
+                j++;
+                continue;
+            }
+            
+            if(a[i]<b[j]){
+                result.push_back(a[i]);
+                i++;
+            }
+            
+            else if(b[j]<a[i]){
+                result.push_back(b[j]);
+                j++;
+            }
+            
+            else{
+                result.push_back(a[i]);
+                i++;
+                j++;
             }
         }
         
+        while(i<n){
+            if(i==0 || a[i]!=a[i-1]){
+                result.push_back(a[i]);
+            }
+                i++;
+        }
+        
+        while(j<m){
+            if(j==0 || b[j]!=b[j-1]){
+                result.push_back(b[j]);
+            }
+                j++;
+        }
+        
         return result;
-        
-        
-        
     }
 };
