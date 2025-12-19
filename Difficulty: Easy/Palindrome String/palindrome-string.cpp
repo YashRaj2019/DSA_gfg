639216f0fce2eb438198de92502308df0bc5b319
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2025-12-19 21:02:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+       int n = s.size();
+       
+       for(int i=0;i<n/2;i++){
+           if(s[i]!= s[n - i - 1]){
+               return false;
+           }
+       }
+       return true;
+    }
+       
+};
