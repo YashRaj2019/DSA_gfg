bc072ededca85d9cd7cac391aacc13a18349cafe
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2025-12-19 20:39:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram of String/anagram-of-string.cpp
+++ Difficulty: Basic/Anagram of String/anagram-of-string.cpp
@@ -2,21 +2,24 @@
 // to be removed to make two strings anagram
 int remAnagram(string s1, string s2) {
     // Your code goes here
-    int f1[26]={0}, f2[26]={0};
     
-    for(char c : s1){
-       f1[c-'a']++; 
-    }
-    for(char c : s2){
-        f2[c-'a']++;
+    int freq1[26] = {0};
+    int freq2[26] = {0};
+    
+    for(int i=0;i<s1.size();i++){
+        freq1[s1[i] - 'a']++;
     }
     
-    int del =0;
+    for(int i=0;i<s2.size();i++){
+        freq2[s2[i] - 'a']++;
+    }
+    
+    int del = 0;
     
     for(int i=0;i<26;i++){
-        del += abs(f1[i] - f2[i]);
+        del += abs(freq1[i] - freq2[i]);
     }
+    
     return del;
     
-};
-
+}
