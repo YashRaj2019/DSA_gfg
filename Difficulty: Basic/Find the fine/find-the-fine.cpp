5ed2c5c4c09a85de75b5e55afab0dc73afa9291e
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-01-22 01:11:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Find the fine/find-the-fine.cpp
+++ Difficulty: Basic/Find the fine/find-the-fine.cpp
@@ -4,25 +4,18 @@ class Solution {
   public:
     long long int totalFine(int date, vector<int> &car, vector<int> &fine) {
         // code here
-        long long int collection = 0;
+        int total = 0;
         
-        int n=car.size();
-        
-        if(date%2==1){
-            for(int i=0;i<n;i++){
-                if(car[i]%2==0){
-                    collection = collection+fine[i];
-                }
+        for(int i = 0; i<car.size(); i++){
+            if(date%2==0 && car[i]%2 != 0){
+                total += fine[i];
+            }
+            
+            else if(date%2 !=0 && car[i]%2==0){
+                total +=  fine[i];
             }
         }
         
-        if(date%2==0){
-            for(int i=0;i<n;i++){
-                if(car[i]%2==1){
-                    collection = collection+fine[i];
-                }
-            }
-        }
-        return collection;
+        return total;
     }
 };
