91532ef1b17759e96ea94b4cddd2fd66cfff8114
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-01-27 17:19:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement stack using array/implement-stack-using-array.cpp
@@ -0,0 +1,58 @@
+class myStack {
+  public:
+  
+    int *arr;
+    int top;
+    int size; 
+    
+    myStack(int n) {
+        // Define Data Structures
+        size = n;
+        arr = new int[n];
+        top = -1;
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        
+        if(top==-1){
+            return true;
+        }
+        return false;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        
+        if(top == size-1){
+            return true;
+        }
+        
+        return false;
+        
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        if(top<size-1){
+            top++;
+            arr[top] = x;
+        }
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        if(top>=0){
+            top--;
+        }
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        
+        if(top>=0){
+            return arr[top];
+        }
+        return -1;
+    }
+};
