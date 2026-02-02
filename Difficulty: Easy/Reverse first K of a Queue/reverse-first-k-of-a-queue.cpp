4207340c775c3f1f3236581bbebc65e55d2614c0
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-02-02 23:48:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse first K of a Queue/reverse-first-k-of-a-queue.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    queue<int> reverseFirstK(queue<int> q, int k) {
+        // code here
+        
+        if(k>q.size()){
+            return q;
+        }
+        
+        stack<int>st;
+        
+        for(int i=0;i<k;i++){
+            st.push(q.front());
+            q.pop();
+        }
+        
+        int n = q.size();
+        
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        
+        while(n--){
+            q.push(q.front());
+            q.pop();
+        }
+        
+        return q;
+    }
+};
