df273d5b8f931d96b120d4b16c86dcbd862b8479
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-02-12 23:37:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -15,31 +15,63 @@ public:
 
 class Solution {
   public:
-    vector<int> leftView(Node *root) {
-        // code here
-        queue<Node*>q;
-        q.push(root);
-        
-        vector<int>ans;
-        
-        while(!q.empty()){
-            int n = q.size();
-            ans.push_back(q.front()->data);
-            
-            while(n--){
-                Node *temp = q.front();
-                q.pop();
-                
-                if(temp->left){
-                    q.push(temp->left);
-                }
-                
-                if(temp->right){
-                    q.push(temp->right);
-                }
-            }
+  
+    // method 2: using recursion
+    
+    void lview(Node *root, int level, vector<int>&ans){
+        if(!root){
+            return;
         }
         
+        if(level==ans.size()){
+            ans.push_back(root->data);
+        }
+        
+        lview(root->left, level+1, ans);
+        lview(root->right, level+1, ans);
+    }
+    vector<int> leftView(Node *root) {
+        // code here
+        
+        // method 1 using queue
+        
+        // queue<Node*>q;
+        // q.push(root);
+        
+        // vector<int>ans;
+        
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     ans.push_back(q.front()->data);
+            
+        //     while(n--){
+        //         Node *temp = q.front();
+        //         q.pop();
+                
+        //         if(temp->left){
+        //             q.push(temp->left);
+        //         }
+                
+        //         if(temp->right){
+        //             q.push(temp->right);
+        //         }
+        //     }
+        // }
+        
+        // return ans;
+        
+        // method 2: using recursion
+        
+        
+        
+        vector<int>ans;
+        if(!root){
+            return ans;
+        }
+        
+        lview(root,0,ans);
         return ans;
+        
+        
     }
 };
