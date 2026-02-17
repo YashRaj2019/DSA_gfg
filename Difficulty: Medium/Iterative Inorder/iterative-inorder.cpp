d0b62161b7157d0d15ea307087e00245d23872a8
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-02-17 16:57:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -10,43 +10,84 @@ class Solution {
   public:
     vector<int> inOrder(Node* root) {
         // code here
-        stack<Node*>s;
-        stack<bool>visited;
-        s.push(root);
-        visited.push(0);
+        
+        // method 1
+        
+        // stack<Node*>s;
+        // stack<bool>visited;
+        // s.push(root);
+        // visited.push(0);
+        
+        // vector<int>ans;
+        
+        // while(!s.empty()){
+        //     Node *temp = s.top();
+        //     s.pop();
+            
+        //     bool flag = visited.top();
+        //     visited.pop();
+            
+        //     // first time visit
+        //     if(flag==0){
+                
+        //         // right
+        //         if(temp->right){
+        //             s.push(temp->right);
+        //             visited.push(0);
+        //         }
+        //         // node
+        //         s.push(temp);
+        //         visited.push(1);
+        //         // left
+        //         if(temp->left){
+        //             s.push(temp->left);
+        //             visited.push(0);
+        //         }
+        //     }
+        //     // second time visit
+        //     else{
+        //         ans.push_back(temp->data);
+        //     }
+            
+        // }
+        // return ans;
+        
+        // method 2
         
         vector<int>ans;
         
-        while(!s.empty()){
-            Node *temp = s.top();
-            s.pop();
-            
-            bool flag = visited.top();
-            visited.pop();
-            
-            // first time visit
-            if(flag==0){
-                
-                // right
-                if(temp->right){
-                    s.push(temp->right);
-                    visited.push(0);
-                }
-                // node
-                s.push(temp);
-                visited.push(1);
-                // left
-                if(temp->left){
-                    s.push(temp->left);
-                    visited.push(0);
-                }
+        while(root){
+            // left part does not exist
+            if(!root->left){
+               ans.push_back(root->data);
+               root = root->right;
             }
-            // second time visit
+            // left part exist
+            
             else{
-                ans.push_back(temp->data);
+                
+                Node *curr = root->left;
+                
+                while(curr->right && curr->right != root){
+                    curr = curr->right;
+                }
+                
+                // left subtree not traversed
+                if(curr->right==NULL){
+                    curr->right = root;
+                    root = root->left;
+                }
+                 //already traversed
+                else{
+                    curr->right = NULL;
+                    ans.push_back(root->data);
+                    root = root->right;
+                }
+               
             }
             
         }
+        
         return ans;
     }
 };
