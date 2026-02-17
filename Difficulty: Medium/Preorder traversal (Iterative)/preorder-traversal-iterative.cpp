2b8d58e6601527085efb98232a58e9691c8139f3
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-02-17 17:04:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
+++ Difficulty: Medium/Preorder traversal (Iterative)/preorder-traversal-iterative.cpp
@@ -15,22 +15,60 @@ class Solution {
   public:
     vector<int> preOrder(Node* root) {
         // code here
-        stack<Node*>s;
-        s.push(root);
+        // stack<Node*>s;
+        // s.push(root);
+        // vector<int>ans;
+        
+        // while(!s.empty()){
+        //     Node *temp = s.top();
+        //     s.pop();
+            
+        //     ans.push_back(temp->data);
+            
+        //     if(temp->right){
+        //         s.push(temp->right);
+        //     }
+            
+        //     if(temp->left){
+        //         s.push(temp->left);
+        //     }
+            
+        // }
+        
+        // return ans;
+        
+        // method 2
+        
         vector<int>ans;
         
-        while(!s.empty()){
-            Node *temp = s.top();
-            s.pop();
-            
-            ans.push_back(temp->data);
-            
-            if(temp->right){
-                s.push(temp->right);
+        while(root){
+            // left part does not exist
+            if(!root->left){
+               ans.push_back(root->data);
+               root = root->right;
             }
+            // left part exist
             
-            if(temp->left){
-                s.push(temp->left);
+            else{
+                
+                Node *curr = root->left;
+                
+                while(curr->right && curr->right != root){
+                    curr = curr->right;
+                }
+                
+                // left subtree not traversed
+                if(curr->right==NULL){
+                    ans.push_back(root->data);
+                    curr->right = root;
+                    root = root->left;
+                }
+                 //already traversed
+                else{
+                    curr->right = NULL;
+                    root = root->right;
+                }
+               
             }
             
         }
