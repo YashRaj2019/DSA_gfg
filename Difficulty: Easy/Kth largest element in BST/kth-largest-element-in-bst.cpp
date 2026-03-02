ed3e17ecde7ae6c68cfdb9f781ab408e49d0b6cd
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-02 16:35:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth largest element in BST/kth-largest-element-in-bst.cpp
@@ -0,0 +1,43 @@
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution {
+  public:
+    
+    void kLargest(Node *root, int &ans, int &k){
+        if(!root){
+            return;
+        }
+        
+        kLargest(root->right, ans, k);
+        k--;
+        if(k==0){
+            ans = root->data;
+        }
+        
+        if(k<=0){
+            return;
+        }
+        
+        kLargest(root->left, ans, k);
+    }
+    
+    int kthLargest(Node *root, int k) {
+        // Your code here
+        
+        int ans;
+        kLargest(root, ans, k);
+        return ans;
+    }
+};
