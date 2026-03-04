9c5442417ccf641abb54befe5d0c2d9f9cba9829
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-04 15:45:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Preorder Traversal and BST/preorder-traversal-and-bst.cpp
@@ -0,0 +1,39 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int canRepresentBST(int arr[], int N) {
+        // code here
+        stack<int>lower, upper;
+        lower.push(INT_MIN);
+        upper.push(INT_MAX);
+        int left, right;
+        
+        for(int i=0;i<N;i++){
+            
+            if(arr[i]<lower.top()){
+                return 0;
+            }
+            
+            while(arr[i]>upper.top()){
+                upper.pop();
+                lower.pop();
+            }
+            
+            left = lower.top();
+            right = upper.top();
+            
+            lower.pop();
+            upper.pop();
+            
+            lower.push(arr[i]);
+            upper.push(right);
+            
+            lower.push(left);
+            upper.push(arr[i]);
+            
+        }
+        
+        return 1;
+    }
+};
