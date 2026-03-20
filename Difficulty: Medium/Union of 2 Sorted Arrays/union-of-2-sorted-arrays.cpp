3fcfd33f4f4db523712b17cab854f18afe808e3d
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-21 04:10:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -2,57 +2,18 @@ class Solution {
   public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         // code here
-        int n=a.size();
-        int m=b.size();
+        set<int>s;
         
-        vector<int>result;
-        
-        int i=0, j=0;
-        
-        while(i<n && j<m){
-            
-            
-            if(i>0 && a[i]==a[i-1]){
-                i++;
-                continue;
-            }
-            
-            if(j>0 && b[j]==b[j-1]){
-                j++;
-                continue;
-            }
-            
-            if(a[i]<b[j]){
-                result.push_back(a[i]);
-                i++;
-            }
-            
-            else if(b[j]<a[i]){
-                result.push_back(b[j]);
-                j++;
-            }
-            
-            else{
-                result.push_back(a[i]);
-                i++;
-                j++;
-            }
+        for(int i=0;i<a.size();i++){
+            s.insert(a[i]);
+        }
+        for(int i=0;i<b.size();i++){
+            s.insert(b[i]);
         }
         
-        while(i<n){
-            if(i==0 || a[i]!=a[i-1]){
-                result.push_back(a[i]);
-            }
-                i++;
-        }
+        // convert set to vector
+        vector<int>ans(s.begin(), s.end());
         
-        while(j<m){
-            if(j==0 || b[j]!=b[j-1]){
-                result.push_back(b[j]);
-            }
-                j++;
-        }
-        
-        return result;
+        return ans;
     }
 };
