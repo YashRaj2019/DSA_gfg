0ab960fc36cc8ed4a3d658946ec1b22c3a885904
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-28 02:42:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        set<int>s;
+        
+        for(int i=0;i<a.size();i++){
+            s.insert(a[i]);
+        }
+        
+        for(int i=0;i<b.size();i++){
+            s.insert(b[i]);
+        }
+        
+        vector<int>ans(s.begin(), s.end());
+        return ans;
+    }
+};
