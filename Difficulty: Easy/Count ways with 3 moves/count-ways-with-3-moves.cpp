16479fb30946442143dd3528f5ed0a24d737bcd9
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-04-29 02:00:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
+++ Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
@@ -1,6 +1,40 @@
 
+// class Solution {
+//   public:
+  
+//   // top down approach
+  
+//     int  ways(int i, vector<int> &dp){
+//         if(i<=1){
+//             return 1;
+//         }
+//         if(i==2){
+//             return 2;
+//         }
+        
+//         if(dp[i] != -1){
+//             return dp[i];
+//         }
+        
+//         return dp[i] = ways(i-1, dp)+ ways(i-2, dp) + ways(i-3, dp);
+//     }
+    
+//     int countWays(int n) {
+//         // your code here
+        
+//         vector<int> dp(n+1, -1);
+        
+        
+//         return ways(n, dp);
+//     }
+// };
+
+
+
 class Solution {
   public:
+  
+  // top down approach
   
     int  ways(int i, vector<int> &dp){
         if(i<=1){
@@ -21,6 +55,15 @@ class Solution {
         // your code here
         
         vector<int> dp(n+1, -1);
-        return ways(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        
+        return dp[n];
     }
 };
