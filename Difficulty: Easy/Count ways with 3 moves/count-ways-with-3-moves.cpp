b0dc2aa64ca262ed8530491edd6613b4ce19d61b
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-13 16:28:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
+++ Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
@@ -1,69 +1,26 @@
 
-// class Solution {
-//   public:
-  
-//   // top down approach
-  
-//     int  ways(int i, vector<int> &dp){
-//         if(i<=1){
-//             return 1;
-//         }
-//         if(i==2){
-//             return 2;
-//         }
-        
-//         if(dp[i] != -1){
-//             return dp[i];
-//         }
-        
-//         return dp[i] = ways(i-1, dp)+ ways(i-2, dp) + ways(i-3, dp);
-//     }
-    
-//     int countWays(int n) {
-//         // your code here
-        
-//         vector<int> dp(n+1, -1);
-        
-        
-//         return ways(n, dp);
-//     }
-// };
-
-
-
 class Solution {
   public:
-  
-  // top down approach
-  
-    int  ways(int i, vector<int> &dp){
-        if(i<=1){
-            return 1;
-        }
-        if(i==2){
-            return 2;
-        }
-        
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        
-        return dp[i] = ways(i-1, dp)+ ways(i-2, dp) + ways(i-3, dp);
-    }
-    
     int countWays(int n) {
         // your code here
-        
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        if(n<=2){
+            return n;
         }
         
+        vector<int>dp(4);
         
-        return dp[n];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
+        for(int i=4; i<=n; i++){
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = dp[3];
+            dp[3] = dp[0] + dp[1] + dp[2];
+        }
+        
+        return dp[3];
     }
 };
