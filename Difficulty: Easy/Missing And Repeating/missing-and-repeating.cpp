a9c8a5471beb494cdebe7c2f8c20133816e04be7
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-05 21:06:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -3,17 +3,16 @@ class Solution {
     vector<int> findTwoElement(vector<int>& arr) {
         // code here
         int n = arr.size();
-        
         vector<int>ans(n+1, 0);
+        
         for(int i=0; i<arr.size(); i++){
             ans[arr[i]]++;
         }
         
-
         int repeated = -1;
         int missing = -1;
         
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<=arr.size(); i++){
             if(ans[i] == 2){
                 repeated = i;
             }
