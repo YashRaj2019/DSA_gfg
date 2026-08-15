90585020108bc6d5d7daab56d3d7533df5dd6942
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-15 16:05:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -17,35 +17,37 @@ class Solution {
   public:
     vector<int> topView(Node *root) {
         // code here
-        
-        vector<int>ans;
-        queue<pair<Node*, int>>q; // (node, horizonatl distance(HD))
-        map<int, int>m; // (horizontal distance, node value)
+        queue<pair<Node*, int>>q; // (node, horizontal distance(HD))
+        map<int, int>m; // (HD, node Val)
         
         q.push({root, 0});
         
-        while(q.size() > 0){
-            Node *curr = q.front().first;
-            int currHD = q.front().second;
+        vector<int>ans;
+        
+        while(!q.empty()){
             
+            Node* curr = q.front().first;
+            int currHD = q.front().second;
             q.pop();
             
             if(m.find(currHD) == m.end()){
                 m[currHD] = curr->data;
             }
             
-            if(curr->left != NULL){
+            if(curr->left){
                 q.push({curr->left, currHD-1});
             }
             
-            if(curr->right != NULL){
+            if(curr->right){
                 q.push({curr->right, currHD+1});
             }
+            
         }
         
         for(auto it : m){
             ans.push_back(it.second);
         }
+        
         return ans;
     }
 };
