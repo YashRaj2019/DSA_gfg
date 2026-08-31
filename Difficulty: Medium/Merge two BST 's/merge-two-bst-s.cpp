3192e0f44e38a8d7fd8294da627754dda65f1a71
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-01 02:17:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -1,21 +1,21 @@
-/*
+/* Structure of a Binary Search Tree node
 class Node {
-  public:
+public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = nullptr;
+        right = nullptr;
     }
-};
-*/
+}; */
 
 class Solution {
   public:
   
-    void inorder(Node *root, vector<int>&ans){
+    void inorder(Node* root, vector<int>&ans){
         if(!root){
             return;
         }
@@ -25,36 +25,14 @@ class Solution {
         inorder(root->right, ans);
     }
     
-    vector<int> merge(Node *root1, Node *root2) {
+    vector<int> merge(Node *r1, Node *r2) {
         // code here
-        vector<int>ans1;
-        vector<int>ans2;
-        
-        inorder(root1, ans1);
-        inorder(root2, ans2);
-        
         vector<int>ans;
         
-        int i = 0, j = 0;
+        inorder(r1, ans);
+        inorder(r2, ans);
         
-        while(i<ans1.size() && j<ans2.size()){
-            
-            if(ans1[i]<ans2[j]){
-                ans.push_back(ans1[i++]);
-            }
-            
-            else{
-                ans.push_back(ans2[j++]);
-            }
-        }
-        
-        while(i<ans1.size()){
-            ans.push_back(ans1[i++]);
-        }
-        
-        while(j<ans2.size()){
-            ans.push_back(ans2[j++]);
-        }
+        sort(ans.begin(), ans.end());
         
         return ans;
     }
