212aa07551869fe554a241dcc3a1651d1d6a3ef1
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-06 20:24:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor in BST/predecessor-and-successor-in-bst.cpp
@@ -0,0 +1,70 @@
+/* Structure of a Binary Search Tree node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+  
+    Node* rightMostInLeftSubtree(Node* root){
+        Node* ans;
+        
+        while(root != NULL){
+            ans = root;
+            root = root->right;
+        }
+        return ans;
+    }
+    
+    Node* leftMostInRightSubtree(Node* root){
+        Node* ans;
+        
+        while(root != NULL){
+            ans = root;
+            root = root->left;
+        }
+        return ans;
+    }
+     
+    vector<Node*> findPreSuc(Node* root, int key) {
+        // code here
+        Node* curr = root;
+        Node* pred = NULL;
+        Node* succ = NULL;
+        
+        while(curr != NULL){
+            if(key < curr->data){ 
+                succ = curr;
+                curr = curr->left;
+            }
+            
+            else if(key > curr->data){
+                pred = curr;
+                curr = curr->right;
+            }
+            
+            else{
+                if(curr->left != NULL){
+                    // inorder pred : right most value in left subtree
+                    pred = rightMostInLeftSubtree(curr->left);
+                }
+                
+                if(curr->right != NULL){
+                    // inorder succ : left most value in right subtree
+                    succ = leftMostInRightSubtree(curr->right);
+                }
+                break;
+            }
+        }
+        return {pred, succ};
+    }
+};
