15d5def0656c21bcb357a4cd09051f6ba3a10ff0
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-07 23:52:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,51 +1,50 @@
 class Solution {
-public:
-
-    bool DetectCycle(vector<vector<int>>& adj, int node, int parent, vector<bool>& visited) {
+  public:
+  
+    bool dfs(int node, int parent, vector<vector<int>>& edges, vector<bool>&visited){
         visited[node] = true;
-
-        for (auto neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                if (DetectCycle(adj, neighbor, node, visited)) {
+        
+        for(int x : edges[node]){
+            if(!visited[x]){
+                if(dfs(x, node, edges, visited)){
                     return true;
                 }
             }
-            else if (neighbor != parent) {
+            
+            // already visited and not the parent
+            else if(x != parent){
                 return true;
             }
         }
+        
         return false;
     }
-
+    
     bool isCycle(int V, vector<vector<int>>& edges) {
-
-        // Step 1: Build adjacency list
-        vector<vector<int>> adj(V);
-
-        for (auto &e : edges) {
-            int u = e[0];
-            int v = e[1];
-
+        // Code here
+        
+        vector<vector<int>>adj(V);
+        
+        // create adjacency list
+        
+        for(auto edge : edges){
+            
+            int u = edge[0];
+            int v = edge[1];
+            
             adj[u].push_back(v);
-            adj[v].push_back(u); // undirected graph
-        }
-
-        // Step 2: DFS
-        vector<bool> visited(V, false);
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                if (DetectCycle(adj, i, -1, visited)) {
-                    return true;
+            adj[v].push_back(u);
+        }    
+            vector<bool>visited(V, false);
+            
+            // Graph can have multiple components
+            for(int i=0; i < V; i++){
+                if(!visited[i]){
+                    if(dfs(i, -1, adj, visited)){
+                        return true;
+                    }
                 }
             }
-        }
-
         return false;
     }
 };
-
-
-
-
-
