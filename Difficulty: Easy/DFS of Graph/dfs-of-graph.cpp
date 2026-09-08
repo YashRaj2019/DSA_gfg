774ca08aec40661f4c52ee45b700085e653b4816
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-08 23:55:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,32 +1,69 @@
+// class Solution {
+//   public:
+  
+//     void dfsHelper(int node, vector<vector<int>>&adj, vector<bool>&visited, vector<int>&ans){
+        
+//         visited[node] = true;
+//         ans.push_back(node);
+        
+//         for(int it : adj[node]){
+//             if(!visited[it]){
+//                 dfsHelper(it, adj, visited, ans);
+//             }
+//         }
+//     }
+    
+//     vector<int> dfs(vector<vector<int>>& adj) {
+//         // Code here
+        
+//         int n = adj.size();
+        
+//         vector<int>ans;
+//         vector<bool>visited(n, false);
+        
+//         dfsHelper(0, adj, visited, ans);
+        
+//         return ans;
+        
+//     }
+// };
+
+
+
 class Solution {
   public:
   
-    void dfsHelper(int node, vector<vector<int>>& adj, vector<bool>&visited, vector<int>&ans){
-        
-        visited[node] = true;
-        ans.push_back(node);
-        
-        
-        // visit neighbour in given order
-        for(int it : adj[node]){
-            if(!visited[it]){
-                dfsHelper(it, adj, visited, ans);
-            }
-        }
-        
-    }
-    
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        
         int n = adj.size();
         
         vector<int>ans;
         vector<bool>visited(n, false);
         
-        dfsHelper(0, adj, visited, ans);
+        stack<int>s;
+        
+        s.push(0);
+        
+        while(s.size() > 0){
+            
+            int node = s.top();
+            s.pop();
+            
+            if(visited[node]){
+                continue;
+            }
+            
+            visited[node] = true;
+            ans.push_back(node);
+            
+            for(int i=adj[node].size()-1; i >= 0; i--){
+                if(!visited[adj[node][i]]){
+                    s.push(adj[node][i]);
+                }
+                
+            }
+        }
         
         return ans;
-        
     }
 };
