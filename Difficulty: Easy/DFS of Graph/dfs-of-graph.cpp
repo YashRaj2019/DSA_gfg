b8fc52594fb260f6f0d475130f25ce7f100ed653
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-08 22:59:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,29 +1,32 @@
 class Solution {
   public:
   
-   void dfsHelper(int node, vector<vector<int>>& adj, vector<bool>&visited, vector<int>&ans){
-       visited[node] = true;
-       ans.push_back(node);
-       
-       
-       // visit neighbours in given order
-       for(int it : adj[node]){
-           if(!visited[it]){
-               dfsHelper(it, adj, visited, ans);
-           }
-       }
-   }
-   
+    void dfsHelper(int node, vector<vector<int>>& adj, vector<bool>&visited, vector<int>&ans){
+        
+        visited[node] = true;
+        ans.push_back(node);
+        
+        
+        // visit neighbour in given order
+        for(int it : adj[node]){
+            if(!visited[it]){
+                dfsHelper(it, adj, visited, ans);
+            }
+        }
+        
+    }
+    
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        int v = adj.size();
         
-        vector<bool>visited(v, false);
+        int n = adj.size();
+        
         vector<int>ans;
-        
+        vector<bool>visited(n, false);
         
         dfsHelper(0, adj, visited, ans);
         
         return ans;
+        
     }
 };
