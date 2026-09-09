e54ba0ac6170607b8b3a552e1561a8f6f743b5fc
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-10 01:04:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int findMaximum(vector<int> &arr) {
+        // code here
+        
+        for(int i=0; i<arr.size()-1; i++){
+            if(arr[i] > arr[i+1]){
+                return arr[i];
+            }
+        }
+        
+        return arr[arr.size()-1];
+    }
+};
