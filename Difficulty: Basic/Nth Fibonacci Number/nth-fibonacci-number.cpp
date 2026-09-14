89af5494292316031fc6c11de356da18cd1f1798
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-14 19:17:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        
+        if(n<=1){
+            return n;
+        }
+        
+        int prev2 = 0;
+        int prev1 = 1;
+        
+        for(int i=2; i<=n; i++){
+            int curr = prev2 + prev1;
+            
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
